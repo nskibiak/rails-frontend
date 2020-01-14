1c80916a0e967d25a8d1c65b61c8bf4ef6a16d3e
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-01-14 12:51:11 +0900

    added le wagon navbar with js and installed typed.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,11 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import 'bootstrap';
+import {initUpdateNavbarOnScroll} from '../components/navbar';
+
+initUpdateNavbarOnScroll();
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
