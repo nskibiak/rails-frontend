5ab758abdd3154fd6280da4a84e4b06f273f6355
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-01-14 12:14:38 +0900

    added popper.js jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,4 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+console.log("Hello from app/javascript/packs/application.js!");
